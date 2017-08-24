411aac80c4fc298ddd1fa69d7f19b7975e654396
Zhanybek Seitaliev <zhandev312@gmail.com>
2017-08-24 18:08:26 +0600

    Refactoring code

--- database/migrations/2017_07_09_153119_create_shop_table.php
+++ database/migrations/2017_07_09_153119_create_shop_table.php
@@ -13,25 +13,25 @@ class CreateShopTable extends Migration
      */
     public function up()
     {
-			Schema::create('shops', function (Blueprint $table) {
-				$table->bigIncrements('id');
-				$table->string('name');
-				$table->string('email');
-				$table->string('phone');
-				$table->string('shop_owner');
-				$table->string('domain');
-				$table->string('myshopify_domain');
-				$table->string('country');
-				$table->string('city');
-				$table->string('plan_name');
-				$table->string('country_name')->nullable();
-				$table->string('province')->nullable();
-				$table->string('timezone')->nullable();
-				$table->string('token')->nullable();
-				$table->timestamps();
-				$table->index('myshopify_domain');
-				$table->index('id');
-			});
+        Schema::create('shops', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('shop_owner');
+            $table->string('domain');
+            $table->string('myshopify_domain');
+            $table->string('country');
+            $table->string('city');
+            $table->string('plan_name');
+            $table->string('country_name')->nullable();
+            $table->string('province')->nullable();
+            $table->string('timezone')->nullable();
+            $table->string('token')->nullable();
+            $table->timestamps();
+            $table->index('myshopify_domain');
+            $table->index('id');
+        });
     }
 
     /**
